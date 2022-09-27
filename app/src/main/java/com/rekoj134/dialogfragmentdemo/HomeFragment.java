cef5b8923dd72f8197fa7427d1b7e21f284cf153
Phạm Văn Đạt <phamvandat130402@gmail.com>
2022-09-27 09:15:18 +0700

    update

--- app/src/main/java/com/rekoj134/dialogfragmentdemo/HomeFragment.java
+++ app/src/main/java/com/rekoj134/dialogfragmentdemo/HomeFragment.java
@@ -1,12 +1,19 @@
 package com.rekoj134.dialogfragmentdemo;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
@@ -22,6 +29,7 @@ public class HomeFragment extends Fragment {
 
         HomeFragment fragment = new HomeFragment();
         fragment.setArguments(args);
+
         return fragment;
     }
 
@@ -30,6 +38,129 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.home_fragment, container, false);
 
+        binding.btnDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog alertDialog = new AlertDialog.Builder(getContext())
+                        .setTitle(R.string.thong_bao)
+                        .setMessage("Bạn đang ở Trang chủ?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        })
+                        .create();
+                alertDialog.show();
+            }
+        });
+
+        String[] strings = {"Android", "Kotlin", "IOS", "Flutter"};
+
+        binding.btnSingleDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog alertDialog = new AlertDialog.Builder(getContext())
+                        .setTitle(R.string.lua_chon)
+                        .setSingleChoiceItems(strings, -1, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        })
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ListView listView = ((AlertDialog) dialogInterface).getListView();
+                                Object checkedItem = listView.getAdapter().getItem(listView.getCheckedItemPosition());
+                                Toast.makeText(getContext(), checkedItem.toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        })
+                        .create();
+
+                alertDialog.show();
+            }
+        });
+
+        boolean[] booleans = {true, false, true, false};
+
+        binding.btnMultiDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog alertDialog = new AlertDialog.Builder(getContext())
+                        .setTitle(R.string.lua_chon)
+                        .setMultiChoiceItems(strings, booleans, new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+
+                            }
+                        })
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                StringBuilder str = new StringBuilder();
+                                for (int index = 0; index < strings.length; index++) {
+                                    if (booleans[index]) str.append(strings[index]).append(" ");
+                                }
+                                Toast.makeText(getContext(), str.toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        })
+                        .create();
+
+                alertDialog.show();
+            }
+        });
+
+        binding.btnMenuHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu menu = new PopupMenu(getContext(), view);
+                MenuInflater menuInflater = menu.getMenuInflater();
+                menuInflater.inflate(R.menu.menu_button, menu.getMenu());
+
+                menu.show();
+
+                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        return menuOnClick(menuItem);
+                    }
+                });
+            }
+        });
+
         return binding.getRoot();
+    }
+
+    private boolean menuOnClick(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nmButtonShow:
+                Toast.makeText(getContext(), "Show", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mnButtonHide:
+                Toast.makeText(getContext(), "Hide", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
